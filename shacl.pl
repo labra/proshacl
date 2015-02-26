fb89baafc3d8c119aaaec1e8de269a26a48d03a5
labra <jelabra@gmail.com>
2015-02-26 17:59:27 +0100

    Removed some warnings

--- shacl.pl
+++ shacl.pl
@@ -4,6 +4,9 @@
 
 emptyContext(ctx([],[],[])).
 
+typing(ctx(_,_,Typing),Typing).
+
+
 %% 
 
 matchNode(context(Graph,Schema,Typing), Node, Label, Result) :-
@@ -29,21 +32,23 @@ matchShape(context(_,_,Typing),
 matchShape(Ctx, 
    arc(Arc,M,unbounded), 
    Ts, 
-   result(Typing, [T | Cs], Rs)) :-
+   result(Typing, [T | Cs], Rs)
+   ) :-
     M > 0, M1 is M - 1,
-	removeSet(Ts,T,Ts1)
+    removeSet(Ts,T,Ts1),
     matchArc(Ctx,Arc,T,Typing1),
     matchShape(Ctx,arc(Arc,M1,unbounded),Ts1,result(Cs,Rs,Typing2)),
     combineTypings(Typing1,Typing2,Typing).
 
 % bounded1
 
-matchShape(context(_,_,Typing), 
+matchShape(Ctx,
   arc(Arc,0,N),
   Ts, 
   result(Typing, [], Ts)) :-
     N >= 0,
-    noMatchArcAny(Ts,Ctx,Arc) .
+    noMatchArcAny(Ctx,Ts,Arc),
+    typing(Ctx,Typing) .
 
 % bounded2
 matchShape(Ctx, 
@@ -51,10 +56,10 @@ matchShape(Ctx,
   Ts, 
   result(Typing,[T|Cs],Rs)) :-
     N >= 0, N1 is N - 1,
-	removeTriple(Ts,T,Ts1),
-	matchArc(Ctx,Arc,T,Typing1),
-	matchShape(Ctx,arc(Arc,0,N1),Ts1,result(Cs,Rs,Typin2),
-	combineTypings(Typing1,Typing2,Typing) .
+        removeTriple(Ts,T,Ts1),
+        matchArc(Ctx,Arc,T,Typing1),
+        matchShape(Ctx,arc(Arc,0,N1),Ts1,result(Cs,Rs,Typing2)),
+        combineTypings(Typing1,Typing2,Typing) .
 
 % bounded3
 
@@ -63,35 +68,39 @@ matchShape(Ctx,
   Ts, 
   result(Typing,[T|Cs],Rs)) :-
     M > 0, N >= M, M1 is N - 1, N1 is N - 1,
-	removeTriple(Ts,T,Ts1),
-	matchArc(Ctx,Arc,T,Typing1),
-	matchShape(Ctx,arc(Arc,M1,N1),Ts1,result(Cs,Rs,Typin2),
-	combineTypings(Typing1,Typing2,Typing) .
+        removeTriple(Ts,T,Ts1),
+        matchArc(Ctx,Arc,T,Typing1),
+        matchShape(Ctx,arc(Arc,M1,N1),Ts1,result(Cs,Rs,Typing2)),
+        combineTypings(Typing1,Typing2,Typing) .
 
 % and
 
 matchShape(Ctx,and(E1,E2),Ts,result(Typing,Cs,Rs)) :-
-   matchShape(ctx,E1,Ts,result(Typing1,Cs1,Rs1)),
-   matchShape(ctx,E2,Rs1,result(Typing2,Cs2,Rs)),
+   matchShape(Ctx,E1,Ts,result(Typing1,Cs1,Rs1)),
+   matchShape(Ctx,E2,Rs1,result(Typing2,Cs2,Rs)),
    combineTypings(Typing1,Typing2,Typing),
    union(Cs1,Cs2,Cs) .
 
 % or1
 
-matchShape(Ctx,or(E1,E2),Ts,R) :-
-   matchShape(ctx,E1,Ts,R) .
+matchShape(Ctx,or(E1,_),Ts,R) :-
+   matchShape(Ctx,E1,Ts,R) .
 
 % or2
 
-matchShape(Ctx,or(E1,E2),Ts,R) :-
-   matchShape(ctx,E2,Ts,R) .
+matchShape(Ctx,or(_,E2),Ts,R) :-
+   matchShape(Ctx,E2,Ts,R) .
 
    
 %%%
 
-matchBasicArc(arc(P,V),triple(S,P,O)):-matchValue(V,O) .
+matchBasicArc(arc(P,V),triple(_,P,O)):-matchValue(V,O) .
 
-matchBasicArc(invArc(P,V),triple(S,P,O)):-matchValue(V,S) .
+matchBasicArc(invArc(P,V),triple(S,P,_)):-matchValue(V,S) .
+
+%%%
+
+noMatchArcAny(Ctx,Triples,Arc) :- undefined .
 
 %%% 
 matchValue(_,valueSet(Set),O):-member(O,Set) .
@@ -101,7 +110,7 @@ matchValue(_,valueType(T),O):- undefined .
 matchValue(context(_,_,Typing), valueRef(Label), O):-
   containsType(Typing,X,Label) .
   
-matchValue(Ctx, valueRef(Label), O, Typing) :-
+matchValue(Ctx, valueRef(Label), X, Typing) :-
   notContainsType(Ctx,X,Label),
   matchNode(Ctx,X,Label,result(_,_,Typing)) .
 
@@ -110,14 +119,14 @@ matchValue(Ctx, valueRef(Label), O, Typing) :-
 combineTypings([],Typing,Typing).
 combineTypings([type(N,T)|Ts1], Ts2, Ts) :-
     addType(N,T,Ts2,TsN),
-	combineTypings(Ts1,TsN,Ts).
-	
+        combineTypings(Ts1,TsN,Ts).
+        
 addType(N,T,[],[type(N,[T])]).
 addType(N,T,[type(N,Typing)|Ts],[type(N,TypingN)|Ts]) :-
-	addSet(T,Typing,TypingN) .
+        addSet(T,Typing,TypingN) .
 addType(N,T,[type(N1,Typing1)|Ts],[type(N1,Typing1)|TsN]) :-
-	N \= N1,
-	addType(N,T,Ts,TsN) .
+        N \= N1,
+        addType(N,T,Ts,TsN) .
 
 containsType([type(X,Labels)|_],X,Label):- member(Label,Labels) .
 containsType([type(Y,_)|Xs],X,Label):- 
@@ -128,9 +137,9 @@ containsType([type(Y,_)|Xs],X,Label):-
 addSet(X,[],[X]).
 addSet(X,[X|L],[X|L]) .
 addSet(X,[Y|L],[Y|LN]) :-
-	X \= Y,
-	addSet(X,L,LN).
-	
+        X \= Y,
+        addSet(X,L,LN).
+        
 union(Xs,[],Xs).
 union([X|Xs],Ys,Zs):-
   union(Xs,Ys,Rs) ,
